2ce4dc87eb968726fe7dbf68f76696aabd54cf93
ArgonautAstra <78497325+ArgonautAstra@users.noreply.github.com>
2023-06-01 16:05:15 +0200

    Refactoring STV

--- Progetto Lab. Algoritmi/STV/STV.cpp
+++ Progetto Lab. Algoritmi/STV/STV.cpp
@@ -2,18 +2,18 @@
 #include <climits>
 #include <cstdlib>
 
-using namespace std;
-
 #define V 10
 #define INF INT_MAX
 
+using namespace std;
+
 // A Dynamic programming based function to find the shortest path from u to v with exactly q edges.
-int* shortestPath(int graph[][V], int u, int v, int q) {
+int* shortestPath(int graph[V][V], int u, int v, int q) {
     int A[V][V], B[V][V], sp[q+1], i, j, k, e;
-    short int x = 1;
+    unsigned char x = 1;
 
     for (i = 0; i < V; i++)
-        for (int j = 0; j < V; j++)
+        for ( j = 0; j < V; j++)
             A[i][j] = (graph[i][j] != INF) ? graph[i][j] : INF;
 
     for (e = 2; e <= q; e++, x *= -1)
@@ -30,43 +30,29 @@ int* shortestPath(int graph[][V], int u, int v, int q) {
                         sp[e] = A[u][v];
                     }
 
-    for (int i = 2; i <= q; i++)
+    for (i = 2; i <= q; i++)
         cout << "Total cost of flight(" << i << ") is: " << sp[i] << "$" << endl;
 
     return sp;
 }
 
 int main(){
-    /*
-    int graph[V][V] = {{INF, 1,3,5,INF},
-                        {INF, INF, 1,3,8},
-                        {INF, 2, INF,3,2},
-                        {INF, 7, 4, INF,1},
-                        {INF, INF, INF,INF, INF}};
-    */
+    int graph[V][V];
 
     srand (time(NULL));
 
-    int graph[V][V];
-
     for(int i = 0; i < V; i++)
-        for(int j = 0; j < V; j++)
-            graph[i][j] = INF;
-
-    for(int i = 0; i < V-1; i++)
-        for(int j = 1; j < V; j++)
-            if(i != j)
-                graph[i][j] = rand() % 9 + 1;
-
-    graph[0][V-1] = INF;
+        for(int j = 0; j < V; j++) {
+            if (i != j) graph[i][j] = rand() % 9 + 1;
+            if (i == j || j == 0 || i == V-1)  graph[i][j] = INF;
+            graph[0][V-1] = INF;
+        }
 
     for(int i = 0; i < V; i++, cout <<  endl)
         for (int j = 0; j < V; j++)
             cout << graph[i][j] << "\t";
 
-    cout <<  endl;
-
-    shortestPath(graph, 0, V-1, V-1);
+    cout <<  endl << shortestPath(graph, 0, V-1, V-1);
 
     return 0;
 }
