f00a28a9a35796f6d940f710eeba38a01c96d0cd
valentimarco <gamespy67@gmail.com>
2023-06-03 13:21:11 +0200

    Construction of library

--- Progetto Lab. Algoritmi/main.cpp
+++ Progetto Lab. Algoritmi/main.cpp
@@ -6,40 +6,89 @@
 #include "PathsManager//PathsManager.h"
 #include "Data Structures/IndexMinPQ.h"
 
+#define V 10
+#define INF INT_MAX
+
+// A Dynamic programming based function to find the shortest path from u to v with exactly q edges.
+int* shortestPath(int graph[V][V], int u, int v, int q) {
+    int A[V][V], B[V][V], sp[q+1], i, j, k, e;
+    unsigned char x = 1;
+
+    for (i = 0; i < V; i++)
+        for ( j = 0; j < V; j++)
+            A[i][j] = (graph[i][j] != INF) ? graph[i][j] : INF;
+
+    for (e = 2; e <= q; e++, x *= -1)
+        for (i = 0; i < V; i++)
+            for (j = 0, A[i][0] = INF, B[i][0] = INF; j < V; j++, A[i][j] = INF, B[i][j] = INF)
+                for (k = 0; k < V; k++)
+                    if (x == 1) {
+                        if (graph[i][k] != INF && k != i != j && A[k][j] != INF)
+                            B[i][j] = std::min(B[i][j], graph[i][k] + A[k][j]);
+                        sp[e] = B[u][v];
+                    } else {
+                        if (graph[i][k] != INF && k != i != j && B[k][j] != INF)
+                            A[i][j] = std::min(A[i][j], graph[i][k] + B[k][j]);
+                        sp[e] = A[u][v];
+                    }
+
+    for (i = 2; i <= q; i++)
+        std::cout << "Total cost of flight(" << i << ") is: " << sp[i] << "$" << std::endl;
+
+    return sp;
+}
+
+
+
+
 int main(){
-//    std::vector<std::string> cities;
-//    cities.emplace_back("Palermo");
-//    cities.emplace_back("Roma");
-//    cities.emplace_back("Madrid");
-//    cities.emplace_back("New York");
-//
-//    FlightsGraph g(cities);
-//    g.addFlight(0, 1, 1);
-//    g.addFlight(1, 2, 8);
-//    g.addFlight(1, 3, 2);
-//    g.addFlight(2, 3, 11);
-
-
     std::ifstream file("text.txt");
-    auto g = parseFileToGraph(file);
-    for (const auto& vec: g) {
-        std::cout << vec << std::endl;
-    }
-
-    for (const auto& pair: map) {
-
-        std::cout << "Scenarios: "<< pair.first << std::endl;
-        std::cout << "Numbers of query to calculate: " << std::endl;
-        if (pair.second.empty()) std::cout << "the vector is empty" << std::endl;
-        else {
-            for (auto a: pair.second) {
-
-                std::cout << a << " ";
-            }
-            std::cout << std::endl;
+    auto graphs = parseFileToGraph(file);
+//    for (const auto& vec: graphs) {
+//        std::cout << vec << std::endl;
+//    }
+//
+//    for (const auto& pair: map) {
+//
+//        std::cout << "Scenarios: "<< pair.first << std::endl;
+//        std::cout << "Numbers of query to calculate: " << std::endl;
+//        if (pair.second.empty()) std::cout << "the vector is empty" << std::endl;
+//        else {
+//            for (auto a: pair.second) {
+//
+//                std::cout << a << " ";
+//            }
+//            std::cout << std::endl;
+//        }
+//        std::cout << std::endl;
+//
+//    }
+    for (auto [scenarios, query]: map) {
+        if (query.empty()){
+            std::cout << "No Flights" << std::endl;
+            continue;
         }
-        std::cout << std::endl;
+        auto graph = graphs[scenarios];
+        auto pathsManager = PathsManager(graph);
+        if (!graph.hasCycle())
+            auto [vertices,cost] = pathsManager.absoluteShortestPathDag();
+        else
+            auto [vertices,cost] = pathsManager.absoluteShortestPath();
+        
+        auto min = pathsManager.shortestDistanceToNy();
+        // Delete no bueno queries
+        for(int i = 0; i < query.size(); i++) {
+            auto k = query.at(i);
+            if (k < min) query.erase(query.begin() + i);
+        }
+        auto max = query[query.size()-1];
 
+        shortestPath(graph.toMatrix(),0,V-1,max);
+        
+        
+        
+        
+            
     }
 
 
