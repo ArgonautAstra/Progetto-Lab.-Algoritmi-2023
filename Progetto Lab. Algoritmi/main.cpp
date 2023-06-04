18479af0edbe6f6f16456192d8ea92a2fb3b0382
valentimarco <gamespy67@gmail.com>
2023-06-04 15:53:31 +0200

    Finish project

--- Progetto Lab. Algoritmi/main.cpp
+++ Progetto Lab. Algoritmi/main.cpp
@@ -4,10 +4,9 @@
 #include "FlightGraphs/FlightsGraph.h"
 #include "parse/parse.h"
 #include "PathsManager//PathsManager.h"
-#include "Data Structures/IndexMinPQ.h"
 
-#define V 10
-#define INF INT_MAX
+
+
 
 
 
@@ -25,7 +24,7 @@ int main(){
         
         auto min = pathsManager.shortestDistanceToNy();
         std::cout << "Delete all routes minor then " << min << std::endl;
-        // Delete no bueno queries
+        // Deletes no bueno queries
         for(int i = 0; i < query.size(); i++) {
             auto k = query.at(i);
             if (k < min) query.erase(query.begin() + i);
@@ -46,48 +45,5 @@ int main(){
         
             
     }
-
-
-//    std::vector<std::string> cities;
-//    cities.emplace_back("Palermo");
-//    cities.emplace_back("2");
-//    cities.emplace_back("3");
-//    cities.emplace_back("4");
-//    cities.emplace_back("5");
-//    cities.emplace_back("New York");
-//
-//    FlightsGraph g(cities);
-//
-//    g.addFlight(0, 1, 1);
-//    g.addFlight(0, 2, 1);
-//    g.addFlight(1, 2, 4);
-//    g.addFlight(2, 3, 2);
-//    g.addFlight(2, 5, 7);
-//    g.addFlight(3, 4, 2);
-//    g.addFlight(3, 5, 1);
-//    g.addFlight(4, 5, 1);
-//
-//    PathsManager sps(g);
-//    auto sp = sps.absoluteShortestPathDag();
-//    auto lp = sps.absoluteLongestPathDag();
-//
-//    std::cout << sp.first << " " << sp.second << std::endl;
-//
-//    std::cout << lp.first << " " << lp.second << std::endl;
-//
-//    //g.addFlight(3, 1, 2928);
-//    std::cout << g.hasCycle() << std::endl;
-//    auto spdk = sps.absoluteShortestPath();
-//
-//    std::cout << spdk.first << " " << spdk.second << std::endl;
-//
-//    auto testm = g.toMatrix();
-//
-//    for(int i = 0; i < testm.getV(); i++){
-//        for(int j = 0; j < testm.getV(); j++) {
-//            std::cout << testm.getCostOf(i, j) << " ";
-//        }
-//
-//        std::cout << std::endl;
-//    }
+    
 }
